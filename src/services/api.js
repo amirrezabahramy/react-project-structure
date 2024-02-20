5d6a0d9b3f2e19e986572256de06256bda33408b
amirrezabahramy <amirrezabahramy@gmail.com>
2024-02-21 02:14:50 +0330

    Added http messages

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { HttpStatusCode } from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -11,6 +11,40 @@ export const setHeader = (key, value) => {
 
 export const removeHeader = (key) => {
   delete api.defaults.headers[key];
+};
+
+const {
+  Ok,
+  Created,
+  Accepted,
+  //
+  BadRequest,
+  Unauthorized,
+  Forbidden,
+  NotFound,
+  TooManyRequests,
+  //
+  InternalServerError,
+} = HttpStatusCode;
+
+export const httpMessages = {
+  success: {
+    [Ok]: "درخواست موفقیت آمیز بود.",
+    [Created]: "عملیات با موفقیت انجام شد.",
+    [Accepted]: "درخواست انجام شد.",
+  },
+  error: {
+    client: {
+      [BadRequest]: "مقادیر نامعتبر می باشند.",
+      [Unauthorized]: "احراز هویت ناموفق بود.",
+      [Forbidden]: "دسترسی غیر مجاز.",
+      [NotFound]: "پیدا نشد.",
+      [TooManyRequests]: "درخواست های بیش از حد مجاز.",
+    },
+    server: {
+      [InternalServerError]: "خطای سرور.",
+    },
+  },
 };
 
 export default api;
